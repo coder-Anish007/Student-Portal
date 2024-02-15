8562bae04ae1790ce163e65b0f32c33141adb653
coder-Anish007 <chettrianish112@gmail.com>
2024-02-15 15:09:01 +0530

    Added Service Layer and wiring repository layer

---
+++ src/main/java/com/anish/service/ServiceLayer.java
@@ -0,0 +1,20 @@
+package com.anish.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.anish.model.Student;
+import com.anish.repository.StudentRepositoryImpl;
+
+@Service
+public class ServiceLayer {
+	
+	@Autowired
+	StudentRepositoryImpl sr;
+	
+	public List<Student> gettingAllStudents(){
+		return sr.getAllStudents();
+	}
+}
