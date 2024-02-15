7d120012a27441cd2c12787c78ba64417cd8dea2
coder-Anish007 <chettrianish112@gmail.com>
2024-02-15 23:42:42 +0530

    added mapping for listing all students

--- src/main/java/com/anish/controller/StudentController.java
+++ src/main/java/com/anish/controller/StudentController.java
@@ -1,9 +1,13 @@
 package com.anish.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.anish.model.Student;
 import com.anish.service.ServiceLayer;
 
 @Controller
@@ -15,5 +19,16 @@ public class StudentController {
 	@GetMapping(path = "/home")
 	public String homeHandler() {
 		return "homePage";
+	}
+	
+	@GetMapping(path = "/allStudent")
+	public ModelAndView ListOfStudents() {
+		List<Student> data = sl.gettingAllStudents();
+
+		ModelAndView model = new ModelAndView();
+
+		model.setViewName("allStudent");
+		model.addObject("StudentList", data);
+		return model;
 	}
 }
